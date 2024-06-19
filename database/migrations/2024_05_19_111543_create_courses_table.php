22e648a88ca62fc15332abc787db3f2bb7b68c25
Ayat AL_Helwani <ayat.hlw123@gamil.com>
2024-06-19 22:29:56 +0300

    Eid Mubarak

--- database/migrations/2024_05_19_111543_create_courses_table.php
+++ database/migrations/2024_05_19_111543_create_courses_table.php
@@ -17,8 +17,8 @@ return new class extends Migration {
             $table->text('title');
             $table->string('image_course');
             $table->longText('description');
-            $table->Decimal('cost'); // Just deleted the is_free attribute. If it is free, The cost is 0 and it can be handled by the frontDevs.
-            $table->integer('average_rating')->default(0); // from 1 star to 5..
+            $table->Decimal('cost'); // Just deleted the is_free attribute. If it is free, The cost is 0, and it can be handled by the frontDevs.
+            $table->integer('average_rating')->default(0); // from 1 star to 5.
             $table->boolean('is_reviewed')->default(false);
         });
     }
