4e4c2262b5bc2971e7a41fb92abdd3897db3a3f7
Wiaam409 <wiaam409@gmail.com>
2024-08-13 11:01:08 +0300

    comment response edit

--- app/Services/Course/CommentService.php
+++ app/Services/Course/CommentService.php
@@ -34,7 +34,7 @@ class CommentService
         $comments = $course->comments;
         $course['average_rating'] = $comments->sum('rating') / count($comments);
         $course->save();
-        return ['message' => $course['average_rating'], 'comment' => $comment];
+        return ['message' => 'Comment added successfully', 'comment' => $comment];
     }
 
     /**
